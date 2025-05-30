3e3dfa439e8a2a1aa4e8dead93cd9c96f66bdaec
MB Burch <mb.burch@conductorone.com>
2025-05-30 13:56:10 -0600

    Add additional config options

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,27 +7,36 @@ import (
 var (
 	accessTokenField = field.StringField(
 		"token",
+		field.WithDisplayName("Personal access token"),
 		field.WithDescription("The GitHub access token used to connect to the GitHub API."),
+		field.WithRequired(true),
+		field.WithIsSecret(true),
 	)
 	orgsField = field.StringSliceField(
 		"orgs",
+		field.WithDisplayName("Organizations"),
 		field.WithDescription("Limit syncing to specific organizations."),
 	)
 	instanceUrlField = field.StringField(
 		"instance-url",
+		field.WithDisplayName("GitHub instance URL"),
 		field.WithDescription(`The GitHub instance URL to connect to. (default "https://github.com")`),
 	)
 	appIDField = field.StringField(
 		"app-id",
+		field.WithDisplayName("GitHub App ID"),
 		field.WithDescription("The GitHub App to connect to."),
 	)
 
 	appPrivateKeyPath = field.StringField(
 		"app-privatekey-path",
+		field.WithDisplayName("GitHub App private key (.pem)"),
 		field.WithDescription("Path to private key that is used to connect to the GitHub App"),
+		field.WithIsSecret(true),
 	)
 	syncSecrets = field.BoolField(
 		"sync-secrets",
+		field.WithDisplayName("Sync secrets"),
 		field.WithDescription(`Whether to sync secrets or not`),
 	)
 	fieldRelationships = []field.SchemaFieldRelationship{
@@ -43,11 +52,17 @@ var (
 )
 
 //go:generate go run ./gen
-var Config = field.NewConfiguration([]field.SchemaField{
-	accessTokenField,
-	orgsField,
-	instanceUrlField,
-	syncSecrets,
-	appIDField,
-	appPrivateKeyPath,
-}, field.WithConstraints(fieldRelationships...))
+var Config = field.NewConfiguration(
+	[]field.SchemaField{
+		accessTokenField,
+		orgsField,
+		instanceUrlField,
+		syncSecrets,
+		appIDField,
+		appPrivateKeyPath,
+	},
+	field.WithConstraints(fieldRelationships...),
+	field.WithConnectorDisplayName("GitHub v2"),
+	field.WithHelpUrl("/docs/baton/github-v2"),
+	field.WithIconUrl("/static/app-icons/github.svg"),
+)
