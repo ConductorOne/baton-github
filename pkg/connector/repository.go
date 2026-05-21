e3bf9d3a5477c806ed43b7c4849290114965f90b
Marcus Goldschmidt <marcus.golds@hotmail.com>
2026-05-21 16:49:37 -0400

    add scope to resource for EntitlementExclusionGroup

--- pkg/connector/repository.go
+++ pkg/connector/repository.go
@@ -126,18 +126,23 @@ func (o *repositoryResourceType) List(ctx context.Context, parentID *v2.Resource
 }
 
 func (o *repositoryResourceType) Entitlements(_ context.Context, resource *v2.Resource, _ resourceSdk.SyncOpAttrs) ([]*v2.Entitlement, *resourceSdk.SyncOpResults, error) {
+	return nil, nil, nil
+}
+
+func (o *repositoryResourceType) StaticEntitlements(_ context.Context, _ resourceSdk.SyncOpAttrs) ([]*v2.Entitlement, *resourceSdk.SyncOpResults, error) {
 	rv := make([]*v2.Entitlement, 0, len(repoAccessLevels))
 	for i, level := range repoAccessLevels {
 		rv = append(rv, entitlement.NewPermissionEntitlement(
-			resource,
+			nil,
 			level,
 			entitlement.WithDisplayName(fmt.Sprintf("Repo %s", titleCase(level))),
 			entitlement.WithDescription(fmt.Sprintf("Access to repository in GitHub as %s", level)),
 			entitlement.WithGrantableTo(resourceTypeUser, resourceTypeTeam),
 			entitlement.WithAnnotation(&v2.EntitlementExclusionGroup{
-				ExclusionGroupId: "repository-permission-" + resource.GetId().GetResource(),
+				ExclusionGroupId: "repository",
 				Order:            uint32(i),
 				IsDefault:        level == repoPermissionPush,
+				ScopeToResource:  true,
 			}),
 		))
 	}
