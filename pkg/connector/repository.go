b871fb8c67dd06f12382466d0ffd0e6cf0317609
Marcus Goldschmidt Oliveira <38927366+MarcusGoldschmidt@users.noreply.github.com>
2026-06-02 14:12:41 -0400

    Add exclusion group (#161)
    * add exclusion group
    
    * fix default permission to match docs
    
    * add scope to resource for EntitlementExclusionGroup
    
    * remove default from repository

--- pkg/connector/repository.go
+++ pkg/connector/repository.go
@@ -131,11 +131,19 @@ func (o *repositoryResourceType) Entitlements(_ context.Context, resource *v2.Re
 
 func (o *repositoryResourceType) StaticEntitlements(_ context.Context, _ resourceSdk.SyncOpAttrs) ([]*v2.Entitlement, *resourceSdk.SyncOpResults, error) {
 	rv := make([]*v2.Entitlement, 0, len(repoAccessLevels))
-	for _, level := range repoAccessLevels {
-		rv = append(rv, entitlement.NewPermissionEntitlement(nil, level,
+	for i, level := range repoAccessLevels {
+		rv = append(rv, entitlement.NewPermissionEntitlement(
+			nil,
+			level,
 			entitlement.WithDisplayName(fmt.Sprintf("Repo %s", titleCase(level))),
 			entitlement.WithDescription(fmt.Sprintf("Access to repository in GitHub as %s", level)),
 			entitlement.WithGrantableTo(resourceTypeUser, resourceTypeTeam),
+			entitlement.WithAnnotation(&v2.EntitlementExclusionGroup{
+				ExclusionGroupId: "repository",
+				Order:            uint32(i),
+				IsDefault:        false,
+				ScopeToResource:  true,
+			}),
 		))
 	}
 
